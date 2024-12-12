61df4f511349b2258b2851fc3f30f1bd20d56e2e
mc1008612 <3029784249@qq.com>
2024-12-12 23:15:04 +0800

    视觉寻找板子，加了DEBUG调试

--- src/ucar_nav/scripts/pid_trace.py
+++ src/ucar_nav/scripts/pid_trace.py
@@ -7,194 +7,149 @@ PID Trace test node
 """
 
 import rospy
+import yaml
 from geometry_msgs.msg import Twist   
+import sys
 
+sys.argv.append('debug')
+sys.argv.append('no_timeout')
+if sys.argv[1] == 'debug':
+    DEBUG_MODE = True
+else :DEBUG_MODE = False
+if sys.argv[2] == 'timeout':
+    TIMEOUT = True      
+else :TIMEOUT = False   
 
+NONGOAL = 0
+GOAL_BUT_NOT_TARGET = 1
+FIND_BUT_NOT_ALLIGN = 3
+FIND =4
 
-class PID_Trace(object):
+class PID_BoardTrace_Controller(object):
     def __init__(self):
-        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        #ideal guide param
-        self.ideal_tomid = rospy.get_param('ideal_tomid',0.001)  # ~[0,1]
-        self.ideal_area_rate = rospy.get_param('ideal_area_rate',[0.15,0.2]) #~[pram_default]
+        '''
+        从yaml初始化控制器参数
+        '''
+        try:
+            with open('visual_trace_board.yaml', 'r') as file:
+                config = yaml.safe_load(file)
+        except FileNotFoundError:
+            rospy.logerr("yaml参数文件不存在!")
+        except Exception :
+            rospy.logerr("yaml参数文件读取失败!{Exception}")
+
         #pid velocity param
-        self.align_area_rate_velocity = 0.15
-        self.align_tomid_velocity = 0.3    
-        self.center_tolarance = 0.07  #comp with raw tomid
-        self.find_target_velocity = 0.1
+        self.align_tomid_velocity = config['align_tomid_velocity']
+        self.find_target_velocity = config['find_target_velocity']
         #time out check param
-        self.timeout_duration_tomid = 20  
-        self.timeout_duration_area_rate = 30
-        self.timeout_duration_both = 20 
-        self.timeout_duration_find_target = 60
+        self.timeout_duration_tomid = config['timeout_duration_tomid']
+        self.timeout_duration_find_target = config['timeout_duration_find_target']
+        '''
+        ros服务参数
+        '''
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)#cmd_vel发布底盘控制命令
+        #ideal guide param 从参数服务器中获取
+        self.ideal_tomid = rospy.get_param('ideal_tomid',0.001)  # ~[0,1] 理想中心到目标点直线的距离(归一化)
         self.start_time = rospy.Time.now().to_sec()
 
-        rospy.loginfo("PID Trace node start!")
-        rospy.loginfo("Ideal area_rate"+str(self.ideal_area_rate))
-        rospy.loginfo("Ideal tomid: "+ str(self.ideal_tomid))
-        rospy.loginfo("Align velocity param:" + str(self.align_area_rate_velocity))
-        rospy.loginfo("Align to mid velocity:" + str(self.align_tomid_velocity)+"\n\n\n")
-        
-    def is_timeout(self,duration):
-        if rospy.Time.now().to_sec() - self.start_time < duration:
-            return False
-        else:
-            return True
-    def convert_twist(self,x, y):
+        log_info = (
+            f"PID Trace node start!\n"
+            f"Ideal tomid: {self.ideal_tomid}\n"
+            f"Align to mid velocity: {self.align_tomid_velocity}"
+        )
+        rospy.loginfo(log_info)
+
+    def check_timeout(self,duration):
+        """
+        :param duration: 超时时间
+        :return:null
+        :raise: TimeoutException
+        """
+        if rospy.Time.now().to_sec() - self.start_time > duration:
+            raise TimeoutError(f"Timeout! Duration: {duration}")
+    def convert_twist(self,x, y,z):
+        """
+        :param x: x 线速度
+        :param y: y 线速度
+        :param w: w 角速度
+        :return: Twist
+        """
         twist = Twist()
         twist.linear.x = x
         twist.linear.y = y
+        twist.angular.z = z
         return twist
     
     def find_target(self):
-        #start time
-        self.start_time = rospy.Time.now().to_sec()
-        rospy.set_param('rescue',0) == 0 
-        while  (not (rospy.get_param('rescue',0) == 3 or rospy.get_param('rescue',0) == 4) 
-                                and abs(rospy.get_param('rescue_x'))>self.center_tolarance 
-                                and not self.is_timeout(self.timeout_duration_find_target)) :
-            msg = Twist()
-            msg.angular.z = self.find_target_velocity
-            self.pub.publish(msg)
+
+        rescue_status = rospy.get_param('rescue', NONGOAL)
+
+        if (rescue_status == FIND or 
+            rescue_status == FIND_BUT_NOT_ALLIGN):  # 找到目标
+                
+                self.pub.publish(self.convert_twist(0, 0, 0))
+                return True          
+        elif (rescue_status == NONGOAL or
+            rescue_status == GOAL_BUT_NOT_TARGET):  # 没有找到目标
+                
+                self.pub.publish(self.convert_twist(0, 0, self.find_target_velocity))
+                return False
         else :
-            if not (rospy.get_param('rescue',0) == 3 or rospy.get_param('rescue',0) == 4):
-                rospy.signal_shutdown("Time out find target!")
-            else :
-                rospy.loginfo("Successfully find target!")
-    def try_align_both(self):
-         #start time
-        self.start_time = rospy.Time.now().to_sec()            
-        lower,upper= self.ideal_area_rate[0],self.ideal_area_rate[1]
-        rospy.loginfo("upper:%f,lower:%f\nideal_tomid:%f",upper,lower,self.ideal_tomid)
-        while not self.is_timeout(self.timeout_duration_both):
-            rescue_state = rospy.get_param('rescue',0)
-            if  rescue_state == 4:                                                                         #rescue_state = 4
-                break
-            #if return value from param sever is @RIGHT
-            elif rescue_state == 3:                                                                        #rescue_state = 3
-            ############################################################
-            #caculate x
-                area_rate = rospy.get_param('rescue_area',0)
-                #normalize area_rate
-                if area_rate == 0:
-                    #no result
-                    nomalize_area_rate = 0
-                elif area_rate < lower :
-                    #require x>0,then area_rate >0
-                    nomalize_area_rate = (lower - area_rate)/lower
-                elif area_rate > upper:
-                    #require x<0,then area_rate <0
-                    nomalize_area_rate = (upper - area_rate)/upper  
-                else :
-                    nomalize_area_rate = 0
-                    #ideal value
-                pid_velocity_x = self.align_area_rate_velocity*nomalize_area_rate
-                #############################################################
-                #calculate y
-                tomid = rospy.get_param('rescue_x')
-                #normalize tomid
-                normalize_tomid = tomid/0.5
-                rospy.loginfo('tomid=%f,normalize_tomid=%f',tomid,normalize_tomid)
-                if abs(normalize_tomid) > self.ideal_tomid:   
-                    pid_velocity_y = -self.align_tomid_velocity*normalize_tomid
-                else :
-                    pid_velocity_y = 0 
-                #############################################################
-                self.pub.publish(self.convert_twist(pid_velocity_x, pid_velocity_y))
-            else:                                                                                          #rescue_state = 0|1
-                continue 
-        else :                                                  
-            rospy.loginfo("Time out Align to area_rate and tomid!")
-            return False
-            
-        rospy.loginfo("Successfully Align to area_rate and tomid!")       
-        return True
 
-    def try_align_tomid(self):
+            rospy.signal_shutdown("错误的rescue码值")
+            exit(1)
 
-        self.start_time = rospy.Time.now().to_sec()            
-        while not self.is_timeout(self.timeout_duration_tomid):
+
+    def pub_tomid_velocity(self):
+        try:
             tomid = rospy.get_param('rescue_x')
-            #normalize tomid
-            normalize_tomid = tomid/0.5
-            rospy.loginfo('tomid=%f,normalize_tomid=%f',tomid,normalize_tomid)
-            if abs(normalize_tomid) > self.ideal_tomid:   
-                pid_velocity_y = -self.align_tomid_velocity*normalize_tomid
-            else :
-                pid_velocity_y = 0
-                break
-            self.pub.publish(self.convert_twist(0, pid_velocity_y))                         
-        else :                                                  
-            rospy.loginfo("Time out Align to mid!")
-            return False
-  
-        rospy.loginfo("Successfully Align to mid!")            
-        return True
-
-
-    def try_align_area_rate(self):
-        self.start_time = rospy.Time.now().to_sec()             
-        lower,upper= self.ideal_area_rate[0],self.ideal_area_rate[1]
-        rospy.loginfo("upper:%f,lower:%f",upper,lower)
-        while not self.is_timeout(self.timeout_duration_area_rate):
-            if  rospy.get_param('rescue',3) == 4:
-                break
-            area_rate = rospy.get_param('rescue_area',0)
-            #normalize area_rate
-            if area_rate == 0:
-                #no result
-                nomalize_area_rate = 0
-            elif area_rate < lower :
-                #require x>0,then area_rate >0
-                nomalize_area_rate = (lower - area_rate)/lower
-            elif area_rate > upper:
-                #require x<0,then area_rate <0
-                nomalize_area_rate = (area_rate - upper)/upper
-            else :
-                nomalize_area_rate = 0
-                #ideal value
-            rospy.loginfo('area_rate=%f,normalize_area_rate=%f',area_rate,nomalize_area_rate)
-            pid_velocity_x = self.align_area_rate_velocity*nomalize_area_rate
-            self.pub.publish(self.convert_twist(pid_velocity_x, 0))
-        else :                                                 
-            rospy.loginfo("Time out Align to area_rate!")
-            return False
+        except rospy.ROSException:
+            rospy.logerr("rescue_x参数不存在!")
+            exit(1)
         
-        rospy.loginfo("Successfully Align to area_rate!")       
-        return True
+    # 确保 tomid 在合理范围内
+        if not (0 <= tomid <= self.max_tomid):
+            rospy.logerr("tomid 值超出范围: {}".format(tomid))
+            return False
 
- 
-if __name__ == '__main__':  
+        #归一化tomid ~norm_tomid~[-1,1]
+        norm_tomid = tomid / 0.5
+
+        if abs(norm_tomid) > self.ideal_tomid:#未对齐   
+            #pid 控制速度
+            pid_velocity_y = - self.align_tomid_velocity * norm_tomid
+            self.pub.publish(self.convert_twist(0, pid_velocity_y,0))
+            
+            if DEBUG_MODE:
+                rospy.loginfo(f"pid_velocity_y:{pid_velocity_y},tomid:{tomid}")
+            return False
+        else :  #对齐
+            self.pub.publish(self.convert_twist(0, 0,0))
+
+            if DEBUG_MODE:
+                rospy.logerr("已经对齐")
+            return True                                                
+
+if __name__ == '__main__' and DEBUG_MODE: 
     try:  
-        rospy.init_node('pid_trace.py')
-        pid_trace = PID_Trace()
-        #wait for start test
-        rospy.loginfo("Waiting for start test...")
-        #######################################
-        while not rospy.is_shutdown():
-            while not rospy.get_param("start_test",0) ==1:
-                pass
-            else:
-                rospy.set_param("start_test",0)
-                rospy.loginfo("Start test!")
-            ##########################block#############################
-            pid_trace.find_target()
-            ##########################block#############################
-            if not rospy.get_param('rescue',0) == 4:
-                rospy.loginfo("Align mid")
-                if pid_trace.try_align_tomid():
-            #         rospy.loginfo("Align both")
-            # ##########################block#############################
-            #         if pid_trace.try_align_both():
-            # ##########################block#############################
-                        pass
-                    # else : 
-                    #         rospy.loginfo("Align area_rate and tomid failed!")
-                    #         rospy.logoinfo("PID Trace node terminated!")
-                else :
-                    rospy.loginfo("Align mid failed!")
-            else :
-                rospy.loginfo("Successfully Align to area_rate and tomid!")
-            ######################################
-    except rospy.ROSInterruptException:  
-        rospy.signal_shutdown("PID Trace node terminated!")
+        rospy.init_node('PID_BoardTrace_Controller')
+        pid_controller = PID_BoardTrace_Controller()
+        
+        #寻找目标
+        pid_controller.start_time = rospy.Time.now().to_sec()#记录开始时间
+        while(not pid_controller.find_target()
+              ): 
+                if TIMEOUT:pid_controller.check_timeout(pid_controller.timeout_duration_find_target)
+
+        #对齐
+        pid_controller.start_time = rospy.Time.now().to_sec()#记录开始时间
+        while(not pid_controller.pub_tomid_velocity()
+                ):
+                if TIMEOUT:pid_controller.check_timeout(pid_controller.timeout_duration_find_target)
+        
+    except TimeoutError:
+        rospy.signal_shutdown("超时!")
+        exit(1)
+    except KeyboardInterrupt:  
+        rospy.signal_shutdown("PID BoardTrace Controller node terminated!")
+        exit(2)

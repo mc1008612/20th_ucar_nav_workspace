6823b8e75813861a559792b8adbbeee42311fb21
mc1008612 <3029784249@qq.com>
2024-12-15 02:50:12 +0800

    @fix：采样tomid，时间比例控制增量实现pid控制

--- src/ucar_nav/scripts/pid_trace.py
+++ src/ucar_nav/scripts/pid_trace.py
@@ -23,7 +23,7 @@ class PID_BoardTrace_Controller(object):
         从yaml初始化控制器参数
         '''
         try:
-            with open('visual_trace_board.yaml', 'r') as file:
+            with open('/home/ucar/ucar_ws/src/ucar_nav/scripts/visual_trace_board.yaml', 'r') as file:
                 config = yaml.safe_load(file)
         except FileNotFoundError:
             rospy.logerr("yaml参数文件不存在!")
@@ -31,7 +31,6 @@ class PID_BoardTrace_Controller(object):
             rospy.logerr(f"yaml参数文件读取失败! {e}")
 
         # PID velocity param
-        self.align_tomid_velocity = config['align_tomid_velocity']
         self.find_target_velocity = config['find_target_velocity']
         pid_param = config['tomid_pid_param']
         self.kp_tomid = pid_param[0]  # 添加 kp 参数
@@ -41,7 +40,7 @@ class PID_BoardTrace_Controller(object):
         self.timeout_duration_tomid = config['timeout_duration_tomid']
         self.timeout_duration_find_target = config['timeout_duration_find_target']
         #tomid刷新率
-        self.tomid_change_rate = config['tomid_change_rate']
+        self.tomid_fresh_period = config['tomid_fresh_period']
 
         '''
         ros服务参数
@@ -49,25 +48,20 @@ class PID_BoardTrace_Controller(object):
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)  # cmd_vel发布底盘控制命令
         # ideal guide param 从参数服务器中获取
         self.ideal_tomid = rospy.get_param('ideal_tomid', 0.001)  # ~[0,1] 理想中心到目标点直线的距离(归一化)
-        self.pub_timer_tomid = rospy.Timer(rospy.Duration(0.1), self.pub_tomid_velocity) #10Hz发布速率
-        #开启定时器，然后关闭回调
-        self.pub_timer_tomid.start()
-        self.pub_timer_tomid.shutdown()
+        self.tomid_sample_timer = rospy.Timer(rospy.Duration(self.tomid_fresh_period), self.tomid_sample_timer_callback)
+        self.tomid_sample_timer._shutdown = True
         #任务开始时间
         self.start_time = None
 
         #临时变量
         self.integral = 0.0  # 积分项
         self.previous_error = 0.0  # 上一次的误差
-        self.tomid = 0.0 #记录tomid
-        self.pid_output_velocity_y = 0.0 # 用于记录发布的当前y轴方向上的速度
-        self.pid_output_velocity_y_time = 0.0 #用于记录应该发布速度的持续时间
-        self.tomid_pub_time_stamp = None
 
         log_info = (
             f"PID Trace node start!\n"
             f"Ideal tomid: {self.ideal_tomid}\n"
-            f"Align to mid velocity: {self.align_tomid_velocity}"
+            f"tomid_fresh_peroid:{self.tomid_fresh_period}\n"
+            f"p&i&d:{self.kp_tomid}\t,{self.ki_tomid},\t{self.kd_tomid}"
         )
         rospy.loginfo(log_info)
 
@@ -112,49 +106,51 @@ class PID_BoardTrace_Controller(object):
             rospy.signal_shutdown("错误的rescue码值")
             exit(1)
 
-    def pub_tomid_velocity(self):
-        self.pub.publish(self.convert_twist(0,self.pid_output_velocity_y,0))
 
-    def change_tomid_velocity(self):
+    def tomid_sample_timer_callback(self,event):
+        """
+        @Describe: 定时器的回调函数,控制每个采样周期位置移动的增量:
+        delta_y = v* t
+        使用参数:P控制:v = Kp*tomid
+        定时采样tomid,更新pid_output_velocity_y
+        在采样后创建一段时间,发布速度
+        如果速度小于0.1.则控制发布的时间，达到控制增量的目的
+        需要注意：
+        采样间隔需要大于tomid更新时间间隔
+        @Returns: null
+        """
         # 获取当前 self.tomid 值
         try:
-            now_tomid = rospy.get_param('rescue_x',0)
-            if(self.tomid == now_tomid #检查tomid更新
-               and now_tomid != 0):#排除初值&对齐情况
+            tomid = rospy.get_param('rescue_x')
+            self.rescue_status = rospy.get_param('rescue')
+            if self.rescue_status == NONGOAL:
                 raise Exception
         except rospy.ROSException:
             rospy.logerr("rescue_x 参数不存在!")
-            return False                                                                                        #程序出口:参数不存在
+            return
         except Exception:
-            rospy.logerr("rescue_x 参数没有更新!")
-            #rescue_x参数不更新，不执行
-            return False                                                                                         #程序出口:参数不更新
+            rospy.logerr("LOST")
+            return                                                                                        #程序出口:参数不存在
 
-            
-        # tomid变更时处理:
-        self.tomid = now_tomid
         # 确保 self.tomid 在合理范围内 (-0.5 到 0.5)
-        if not (-0.5 <= self.tomid <= 0.5):
-            rospy.logerr(f"self.tomid 值超出范围: {self.tomid}")
-            return False                                                                                          #程序出口:tomid 超出范围
+        if not (-0.5 <= tomid <= 0.5):
+            rospy.logerr(f"self.tomid 值超出范围: {tomid}")
+            return                                                                                                #程序出口:tomid 超出范围
 
         # 归一化 self.tomid 到 [-1, 1]
-        normalized_tomid = self.tomid / 0.5                                             #tomid合理，使用值进行控制更新
+        normalized_tomid = tomid / 0.5                                               #tomid合理，使用值进行控制更新
 
         # 检查是否对齐
         if abs(normalized_tomid) <= self.ideal_tomid:
-            self.pub.publish(self.convert_twist(0, 0, 0))
+            self.pub.publish(self.convert_twist(0, 0, 0))#停车
             # 重置积分项和上一次的误差
             self.integral = 0.0
             self.previous_error = 0.0
             if DEBUG_MODE:
                 rospy.loginfo("已经对齐")
-            return True                                                                                           # 程序出口:已对齐
+            return                                                                                                       # 程序出口:已对齐
 
-        #未对齐：                                                                                                   进行状态更改
-
-        #先关闭发送定时器
-        self.pub_timer_tomid._shutdown = True
+        #未对齐：                                                                                                   对delta_y进行pid
 
         # 计算PID 控制速度
         error = normalized_tomid
@@ -162,41 +158,35 @@ class PID_BoardTrace_Controller(object):
         derivative = error - self.previous_error
         self.previous_error = error
 
-        self.pid_output_velocity_y = -   (self.kp_tomid * error + 
+        pid_output_velocity_y =  - (self.kp_tomid * error + 
                                   self.ki_tomid * self.integral + self.kd_tomid * derivative)
         
         #针对小于0.1的pid输出，做时间pid补偿
-        if(self.pid_output_velocity_y<0.1):
+        if(abs(pid_output_velocity_y)<0.1):
             """
-            #只在速度小于0.1时,做pid补偿输出,使用时间pid对象
+            #只在速度小于0.1时,做时间控制补偿增量输出
             #param: pid_output_velocity_y_time
             """
-            self.pid_output_velocity_y_time = 0.1*(1/self.tomid_change_rate) / self.pid_output_velocity_y
-            #将发布时间速度置回0.1
-            self.pid_output_velocity_y = 0.1
+            pid_output_velocity_y_time = (abs(pid_output_velocity_y)/0.1) * self.tomid_fresh_period
+            #将发布时间速度置回+-0.1
+            pid_output_velocity_y = 0.1* pid_output_velocity_y/abs(pid_output_velocity_y)
 
-            #使用定时器发布
-            #如果时间戳为空，创建时间戳
-            if self.tomid_pub_time_stamp == None:
-                self.tomid_pub_time_stamp = rospy.Time.now().to_sec()
-            self.pub_timer_tomid._shutdown = False  #开启定时器发送速度指令
-            if(not #如果超过设定的发送时间，则停止定时器
-            rospy.Time.now().to_sec() - self.tomid_pub_time_stamp > self.pid_output_velocity_y_time):
-                self.pub.publish(self.convert_twist(0,0,0))#停车
-                self.pub_timer_tomid._shutdown = True#关闭定时器回调发送速度指令
-                self.tomid_pub_time_stamp = None#清空时间戳
-
-        else :#pid速度大于0.1的情况
-            #开启定时器回调函数一直发送
-            self.pub_timer_tomid._shutdown = False
-
+        else :#pid速度大于0.1的情况:跑满整个间隔
+            pid_output_velocity_y_time = self.tomid_fresh_period
+            pass
+        
         #DEBUG调试终端显示
         if DEBUG_MODE:
-            rospy.loginfo(f"self.pid_output_velocity_y: {self.pid_output_velocity_y}, self.tomid: {self.tomid}")
-        return False
+            rospy.loginfo(f"pid_output_velocity_y: {pid_output_velocity_y}, \
+            time:{pid_output_velocity_y_time}, \t tomid: {tomid}")
+
+        #创建一段时间通过速度控制增量
+        time_stamp = rospy.Time.now().to_sec()
+        while(rospy.Time.now().to_sec() - time_stamp <pid_output_velocity_y_time):
+            self.pub.publish(self.convert_twist(0,pid_output_velocity_y,0))
 
 
-if __name__ == '__main__' and DEBUG_MODE: 
+if __name__ == '__main__' and DEBUG_MODE:
     try:  
         rospy.init_node('PID_BoardTrace_Controller')
         pid_controller = PID_BoardTrace_Controller()
@@ -209,10 +199,12 @@ if __name__ == '__main__' and DEBUG_MODE:
 
         # 对齐
         pid_controller.start_time = rospy.Time.now().to_sec()  # 记录开始时间
-        while not pid_controller.change_tomid_velocity():
-            if TIMEOUT:
-                pid_controller.check_timeout(pid_controller.timeout_duration_tomid)
-        
+        pid_controller.tomid_sample_timer._shutdown = False
+        if TIMEOUT:
+            while(not pid_controller.check_timeout(pid_controller.timeout_duration_tomid)):
+                pass
+        rospy.spin()
+
     except TimeoutError:
         rospy.signal_shutdown("超时!")
         exit(1)
